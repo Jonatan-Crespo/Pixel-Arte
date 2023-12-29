1554e12b6767a2be4fc1ec533881d9d61b18ad99
Jonatan Crespo <99922163+Jonatan-Crespo@users.noreply.github.com>
2023-12-28 22:55:44 -0300

    Subindo os arquivos iniciais do projeto

---
+++ app.js
@@ -0,0 +1,67 @@
+
+const colors = document.getElementsByClassName('color');
+colors[0].style.backgroundColor = 'black';
+const color1 = document.getElementById('color1');
+const color2 = document.getElementById('color2');
+const color3 = document.getElementById('color3');
+
+//botão random cores
+const botaoCores = document.getElementById('buttom-random-color');
+botaoCores.addEventListener('click', (event) => {
+    function randomColors() {
+        let colorsRandom = [];
+        for (let i = 0; i < 3; i++) {
+            const letters = '0123456789ABCDEF';
+            let cor = '#';
+            for (let i = 0; i < 6; i++) {
+                cor += letters[Math.floor(Math.random() * 16)];
+            }
+            colorsRandom.push(cor);
+        }
+        return colorsRandom;
+    }
+    event = randomColors();
+    setBackgroundColor(event);
+    console.log(event);
+});
+
+//Aplicação da cor e salvar no local storage
+function setBackgroundColor(color) {
+    color1.style.backgroundColor = color[0];
+    color2.style.backgroundColor = color[1];
+    color3.style.backgroundColor = color[2];
+    localStorage.setItem('backgroundColor1', color[0]);
+    localStorage.setItem('backgroundColor2', color[1]);
+    localStorage.setItem('backgroundColor3', color[2]);
+}
+
+// // Voltar as cores salvas no local Storage
+// const initialize = () => {
+//     const backgroundColor1 = localStorage.getItem('backgroundColor1');
+//     setBackgroundColor(backgroundColor1);  
+// }
+// initialize();
+
+//Criar as divs para o quadro 5x5
+function createDiv() {
+    const section = document.getElementById('pixel-board');
+    for (let i = 0; i < 5; i++) {
+        const divLinha = document.createElement('div');
+        divLinha.innerHTML = '';;
+        divLinha.className = 'linha';
+        section.appendChild(divLinha);
+
+        for (let z = 0; z < 5; z++) {
+            const div = document.createElement('div');
+            div.innerHTML = '';
+            div.className = 'pixel';
+            divLinha.appendChild(div);
+        }
+    }
+}
+
+createDiv();
+
+//Selecionar as cores
+const selectColors = document.getElementsByClassName('color')
+selectColors.addEventListener('click', () => { })
