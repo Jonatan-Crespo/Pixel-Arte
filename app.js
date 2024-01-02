19b40ea73ffff7a2d0ca55fed132a4ff4c4cfba1
Jonatan Crespo <jonatan.domingues.crespo@gmail.com>
2024-01-02 10:47:40 -0300

    Quandro e todas as funções já funcionando, salvando as cores no LocalStorage, mas ainda não buscando quando recarrega a pagina

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const color3 = document.getElementById('color3');
 const botaoCores = document.getElementById('buttom-random-color');
 botaoCores.addEventListener('click', (event) => {
     function randomColors() {
-        let colorsRandom = [];
+        let colorsRandom = ['',];
         for (let i = 0; i < 3; i++) {
             const letters = '0123456789ABCDEF';
             let cor = '#';
@@ -22,17 +22,27 @@ botaoCores.addEventListener('click', (event) => {
     }
     event = randomColors();
     setBackgroundColor(event);
+    // console.log(event);
 });
 
-//Aplicação da cor e salvar no local storage
+//Aplicação da cor e salvar no local storage - Refatorado
 function setBackgroundColor(color) {
-    color1.style.backgroundColor = color[0];
-    color2.style.backgroundColor = color[1];
-    color3.style.backgroundColor = color[2];
-    localStorage.setItem('backgroundColor1', color[0]);
-    localStorage.setItem('backgroundColor2', color[1]);
-    localStorage.setItem('backgroundColor3', color[2]);
+    for (let i = 1; i < colors.length; i += 1) {
+        colors[i].style.backgroundColor = color[i];
+        localStorage.setItem('colorPallet', color)
+    };
+    // console.log(color);
 }
+
+//Função antiga para aplicação da cor e salva no local storage
+// function setBackgroundColor(color) {
+//     color1.style.backgroundColor = color[0];
+//     color2.style.backgroundColor = color[1];
+//     color3.style.backgroundColor = color[2];
+//     localStorage.setItem('backgroundColor1', color[0]);
+//     localStorage.setItem('backgroundColor2', color[1]);
+//     localStorage.setItem('backgroundColor3', color[2]);
+// }
 
 // // Voltar as cores salvas no local Storage
 // const initialize = () => {
@@ -68,6 +78,30 @@ for (let index = 0; index < colors.length; index += 1) {
             selected.classList.remove('selected');
         }
         event.target.classList.add('selected');
+        // return colors[index].style.backgroundColor;
+    })
+    // console.log(colors[index]);
+}
+
+//Selecionar o uma div no quadro 5x5
+const pixel = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixel.length; index += 1) {
+    pixel[index].addEventListener('click', (event) => {
+        const selected = document.querySelector('.selected');
+        const backgroundColorPaleta = selected.style.backgroundColor;
+        // console.log(backgroundColorPaleta);
+        if (selected) {
+            event.target.style.backgroundColor = `${backgroundColorPaleta}`
+        }
+        // JSON.parse(localStorage.setItem('pixelBoard', event));
     })
 }
 
+//Botão de limpar o quadro 5x5
+const limpar = document.getElementById('clear-board');
+limpar.addEventListener('click', (event) => {
+    const pixel = document.querySelectorAll('.pixel');
+    pixel.forEach(pixels => {
+        pixels.style.backgroundColor = '';
+    });
+});
